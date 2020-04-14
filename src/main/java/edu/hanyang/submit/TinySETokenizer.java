722b71d51c2b3fc889ab6d678705722032368d47
himchanyoon9212 <55573050+himchanyoon9212@users.noreply.github.com>
2020-04-14 16:36:29 +0900

    edit

--- src/main/java/edu/hanyang/submit/TinySETokenizer.java
+++ src/main/java/edu/hanyang/submit/TinySETokenizer.java
@@ -1,19 +1,54 @@
 package edu.hanyang.submit;
 
 import java.util.List;
-
 import edu.hanyang.indexer.Tokenizer;
 
-public class TinySETokenizer implements Tokenizer {
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
 
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.tartarus.snowball.ext.PorterStemmer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+
+public class TinySETokenizer implements Tokenizer {
+	SimpleAnalyzer analyzer;
+	PorterStemmer stemmer;
+	List<String> output = new ArrayList<String>();
+	
 	public void setup() {
+		analyzer = new SimpleAnalyzer();
+		stemmer = new PorterStemmer();
 	}
 
 	public List<String> split(String text) {
-		return null;
+		try {
+			TokenStream stream = analyzer.tokenStream(null, new StringReader(text));
+			stream.reset();
+			CharTermAttribute term = stream.getAttribute(CharTermAttribute.class);
+			
+			while (stream.incrementToken()) {
+				String temp = term.toString();
+				stemmer.setCurrent(term.toString());
+				stemmer.stem();
+				String input = stemmer.getCurrent();
+				output.add(input);
+				
+			}
+			stream.close();
+			
+		} catch (IOException e){
+			throw new RuntimeException(e);
+		}
+		
+		return output;
 	}
 
 	public void clean() {
+		
+		output.clear();
+		
 	}
 
 }
